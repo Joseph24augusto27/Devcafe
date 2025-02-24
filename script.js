68d4c5d819353d0aea494a6706888851351efe8e
joseph24augusto27 <josephinho2000@gmail.com>
2025-02-24 12:05:07 -0300

    dev cafe melhorado

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const modal = document.querySelector(".modal")
-const mascara = document.querySelector(".mascara-modal")
+const modal = document.querySelector(".modal");
+const mascara = document.querySelector(".mascara-modal");
+const enderecoLink = document.getElementById("enderecoLink");
 
+enderecoLink.addEventListener("click", function (e) {
+    e.preventDefault();  // Previne o comportamento padrão do link
+    mostrarModal();
+});
 
 function mostrarModal() {
-    mascara.style.visibility = "visible"
-    modal.style.left = "50%"
+    mascara.style.visibility = "visible";
+    mascara.style.opacity = "1";
+    modal.style.transform = "translate(-50%, -50%) scale(1)";
 }
 
 function esconderModal() {
-    mascara.style.visibility = "hidden"
-    modal.style.left = "-30%"
+    mascara.style.visibility = "hidden";
+    mascara.style.opacity = "0";
+    modal.style.transform = "translate(-50%, -50%) scale(0)";
 }
+
+// Fechar o modal ao clicar na máscara
+mascara.addEventListener("click", esconderModal);
